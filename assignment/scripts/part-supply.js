8f9a627b5dc1186a7ab15d9ae903f0af1c1e837a
Sabrina Ciaciura <ciaci0028@gmail.com>
2021-11-11 10:48:06 -0600

    middle push

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -31,9 +31,19 @@ supplyChanges.push(25);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
-for ( let i = 0; i < supplyChanges.length; i++){
+for ( let i = 0; i < supplyChanges.length; i++ ){
   console.log(supplyChanges[i]);
-}
+  if ( supplyChanges[i] > 0 ){
+    console.log( 'Added ' + supplyChanges[i] + ' parts.' );
+  } // end positive number
+  else if ( supplyChanges[i] === 0){
+  console.log( 'No Change.');
+  } // end zero
+  else if ( supplyChanges[i] < 0 ){
+  console.log( 'Removed ' + supplyChanges[i] + ' parts.' );
+  } // end negative number
+} // end for
+
 
 
 // STRETCH GOALS
